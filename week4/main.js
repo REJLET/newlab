c592edab061804ff55423e43b639038382aeeba8
REJLET <ae2035@hw.ac.uk>
2023-11-03 09:52:27 +0400

    added json file

---
+++ week4/main.js
@@ -0,0 +1,23 @@
+var myCity =[
+    {
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Dubai Mall"
+    },
+    {
+        "name": "Milan",
+        "country": "Italy",
+        "place": "Domo di milan"
+    },
+    {
+        "name": "Abu Dhabi",
+        "country": "UAE",
+        "place": "Marina Mall"
+    }
+]
+
+
+console.log(myCity[1].country);
+
+var myCity2 = ["Abu Dhabi", "UAE"];
+console.log(myCity2[1]);
