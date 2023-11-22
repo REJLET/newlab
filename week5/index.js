78d141d6c837c5d91fec488b2a19080f14e8c380
REJLET <ae2035@hw.ac.uk>
2023-11-22 22:11:24 +0400

    updated version with new files and db

--- week5/index.js
+++ week5/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'});
+const path = require('path');
 const app = express();
+const port = 4000;
 
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'nodes-project',
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE,
     socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock',
     port : 8889
 });
@@ -21,12 +25,16 @@ db.connect((error)=>{
     }
 });
 
-const port = 4000;
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 
-app.get('/',(req,res) => {
-    res.send('<h1>HOME PAGE</h1>')
+//define routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
-});
-app.listen(port,()=> {
-    console.log(`Server started on port ${port}`)
-});
+    app.listen(port,()=> {
+        console.log(`Server started on port ${port}`)
+    });
---
+++ week5/routes/pages.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res) => {
+    res.render('index');
+
+});
+router.get('/logout',(req,res) => {
+    res.render('logout');
+
+});
+router.get('/register',(req,res) => {
+    res.render('register');
+
+});
+router.get('/profile',(req,res) => {
+    res.render('profile');
+
+});
+
+module.exports = router;
